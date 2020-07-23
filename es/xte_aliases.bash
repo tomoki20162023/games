6fa1d8c5b4f6558a39b3bf610909bcf0052f843d
tomoki20162023 <tmk.ggl81@gmail.com>
2020-07-23 13:01:59 +0900

    update alias

--- es/xte_aliases.bash
+++ es/xte_aliases.bash
@@ -1,5 +1,4 @@
 alias click='xte "mouseclick 1" "usleep 300000"'
-alias es.roulet='xargs -a arglist/roulet.args xte'
 alias es.menu.bottom='es.menu.select bottom'
 alias es.menu.left='es.menu.select left'
 alias es.menu.right='es.menu.select right'
